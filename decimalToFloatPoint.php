a9f9b7e159bb40af2e815c6db6e97fcfd39f6abd
Paul <polagorge@gmail.com>
2021-12-30 11:38:41 +0100

    added a description for the methods

--- decimalToFloatPoint.php
+++ decimalToFloatPoint.php
@@ -1,8 +1,16 @@
 <?php
-
-function convertToFloatPoint(float $q,float $decmialNumber){
+/**
+ * @param float $q
+ * @param float $decimalNumber
+ * @return false|float
+ *
+ * @description The basic formula (Decimal number) * 2^(Q)
+ * Example : Decimal number : 10.23 ,Q = 2.3
+ * 10.23 * 2^(3) <---- 3 because we ceil 2.3
+ */
+function convertToFloatPoint(float $q,float $decimalNumber){
     $q = ceil($q);
-    return ceil($decmialNumber * pow(2,$q));
+    return ceil($decimalNumber * pow(2,$q));
 }
 
 echo convertToFloatPoint(7.8,5.3) ;
---
+++ decimalToHexDecimal.php
@@ -0,0 +1,29 @@
+<?php
+
+$numbersBiggerThatNine = [
+    10 => 'A',
+    11 => 'B',
+    12 => 'C',
+    13 => 'D',
+    14 => 'E',
+    15 => 'F',
+];
+
+/**
+ * @param int $number
+ * @param $numbersBiggerThatNine
+ * @return string
+ *
+ * @description base number / 16 recursively this has two outputs
+ * 1st output base number / 16 = the recursion
+ * 2nd output base number % 16 = reminder of the division and that is the hex output
+ */
+function convertToHexDecimal(int $number ,$numbersBiggerThatNine): string{
+    $hex = $number % 16;
+    $number = $number / 16;
+    $hex = $hex <= 9 ? $hex : $numbersBiggerThatNine[$hex] ;
+    if ($number <= 0) return '' ;
+    return convertToHexDecimal($number,$numbersBiggerThatNine) . $hex;
+}
+
+echo convertToHexDecimal(188 ,$numbersBiggerThatNine);
